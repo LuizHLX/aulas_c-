a9228f60e63abc6ea81c84cf04ef3c8e8a6d1bf1
Luiz HLX <luiz.lucena0411@gmail.com>
2026-09-09 20:59:45 -0300

    exercicio 4

--- 03.09.26_exercicio_log_prog_lista_4.cpp
+++ 03.09.26_exercicio_log_prog_lista_4.cpp
@@ -1,3 +1,69 @@
-// Exercicio 1
+//=================================
+// Exercicio 1 utilizando do-while
+//=================================
 
-teste do teste
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num, sum;
+    
+    do {
+    cout<<"Insira os números a serem somados: ";
+    cin>>num;
+    
+    sum+=num;
+}
+while (num != 0);
+    
+        cout<<"O a soma dos números digitados é: " <<sum;
+    
+    return 0;
+}
+
+//=================================
+// Exercicio 2 utilizando do-while
+//=================================
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main() {
+    int num_aleatorio, num;
+
+    // Gerando num aleatorio
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1,50);
+    num_aleatorio = distrib(gen);
+    
+    do {
+    cout<<"Insira o número e tente a sorte: ";
+    cin>>num;
+        
+}
+while (num != num_aleatorio);
+      if (num == num_aleatorio) {
+
+    cout<<"Você acertou, o número era: " <<num_aleatorio;
+    }
+    return 0;
+}
+
+
+//=================================
+// Exercicio 3 teste de mesa
+//=================================
+// TESTE DE MESA
+// ------------------------------------------------
+// | i  | x  | Condição (i<=x) | y  | Ação        |
+// ------------------------------------------------
+// | 10 | 15 | 10<=15 = TRUE   | -  | y=15+10=25 |
+// | 15 | 15 | 15<=15 = TRUE   | 25 | y=15+15=30 |
+// | 20 | 15 | 20<=15 = FALSE  | 30 | Fim        |
+// ------------------------------------------------
+// Saída: 30
+// Resposta: b) 30
