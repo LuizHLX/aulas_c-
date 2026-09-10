041c94cad498f5fdb603719668d39d2d8ae7fcc2
Luiz HLX <luiz.lucena0411@gmail.com>
2026-09-10 20:47:47 -0300

    Atualizando exercicio

--- 05_aula_5/10.09.26_exercicio_01.cpp
+++ 05_aula_5/10.09.26_exercicio_01.cpp
@@ -1,75 +1,34 @@
-/*
-Definindo uma Array 
-
-int nota [2] --> Precisa ser sempre do mesmo valor
-
-tipos de dados que podemos incluir dentro da Array:
-- int
-- float
-- char
-
-O tamanho da Array deve ser definido no momento da execução do programa.
-
-Iniciando uma array:
-
-<tipo><nome>[tamanho do vetor]
-
-Exemplo: float v [3]; // declara o vetor "v" com três floats.
-
-como inicializar:
-
-V[0]=4.3
-V[1]=7.0 
-V[2]=-1.2
-
-Outro modo de inicializar
-
-float V[3]={4.3, 7.0, -1.2};
-
-=============================
-// Exemplo 1
+// Exercicio 1
 
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main () {
-	int pares [5];
-	for (int i=0; i<5; i++){
-		pares[i] = 2 * i
-
-	}
-
+    
+    int n=9, i=0;
+    int num_user[n], num_qdr[n];
+    
+    for(i=0; i<10; i++){
+        system("cls");
+        cout<<"Digite o "<<i+1<<" numero: ";
+        cin>>num_user[i];
+    }
+    
+    for (i=0;i<10;i++){
+        num_qdr[i] = num_user[i]*num_user[i];
+    }
+    
+    cout<<endl<<"Lista com numeros digitados: "<<endl;
+    for (i=0;i<10;i++){
+        cout<<num_user[i]<<" ";    
+    }
+    
+    cout<<endl<<endl<<"Lista com quadrados: "<<endl;
+    for (i=0;i<10;i++){
+        cout<<num_qdr[i]<<" ";
+    }
+    
 return 0;
 }
-
-=============================
-// Exemplo 2
-
-#include <iostream>
-
-using namespace std;
-
-int main () {
-	int pares [5];
-	for (int i=0; i<5; i++){
-		pares[i] = 2 * i
-	}
-	for (int i=0; i<5; i++){
-		cout<<pares[i]<<endl;
-
-	}
-
-return 0;
-}
-=============================
-
-O contador é reutilizado, mas sempre zerando o 0 no inicio do looping for
-
-for(i=0;i<4;i++){
-}
-
-
-
-
-*/
